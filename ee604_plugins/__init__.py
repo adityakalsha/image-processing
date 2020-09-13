51c3dacf7b5685702aa7a76f063e402e5cc6adca
adityakalsha <71204080+adityakalsha@users.noreply.github.com>
2020-09-13 12:27:05 +0530

    Update __init__.py

--- ee604_plugins/__init__.py
+++ ee604_plugins/__init__.py
@@ -2,10 +2,11 @@ import numpy as np
 import subprocess
 
 assignment_no_list = [0]
-task_no_list = [3]
-def download_dataset(assignment_no=0, task_no=3):
+task_no_list = [0]
+def download_dataset(assignment_no=0, task_no=0):
     if (assignment_no in assignment_no_list) and (task_no in task_no_list):
-        url = "https://github.com/ee604/ee604_assignments/raw/master/assignment_" + str(assignment_no) + "/data/task_" + str(task_no) + ".zip"
+        #url = "https://github.com/ee604/ee604_assignments/raw/master/assignment_" + str(assignment_no) + "/data/task_" + str(task_no) + ".zip"
+        url = "https://github.com/adityakalsha/EE604Assignments/raw/master/assignment_" + str(assignment_no) + "/data/task_" + str(task_no) + ".zip"
         subprocess.check_output(["wget", "-O", "data.zip", url])
         subprocess.check_output(["unzip", "-o", "data.zip", "-d", "./data/"])
         print("Download Complete!")
